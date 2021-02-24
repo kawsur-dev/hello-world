6964c3cef3c13cb120e9b41ed5c372da747932f5
Mir Md Kawsur <kawsurilu@yahoo.com>
2021-02-24 14:18:54 +0600

    php page redirection

---
+++ PageOneRedirection.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Page One Redirection</title>
+</head>
+<body>
+
+	<h1>Page One Redirection</h1>
+
+</body>
+</html>
---
+++ PageTwoRedirection.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Page Two Redirection</title>
+</head>
+<body>
+
+	<h1>Page Two Redirection</h1>
+
+	<?php 
+		header('Location: PageOneRedirection.php');
+		exit;
+	?>
+
+</body>
+</html>
