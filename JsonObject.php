b4e5820a7b7c6f068d6beb2be5d612d57d7593b2
Mir Md Kawsur <kawsurilu@yahoo.com>
2021-03-06 12:53:22 +0600

    php json complete

--- JsonObject.php
+++ JsonObject.php
@@ -10,22 +10,36 @@
 	<?php 
 
 		$arr1 = array('userId' => 1, 'password' => '123', 'fullName' => "abc");
-		$json_encoded = json_encode($arr1);
+		$json_encoded_1 = json_encode($arr1);
+
+		$arr2 = array('userId' => 2, 'password' => '345', 'fullName' => "def");
+		$json_encoded_2 = json_encode($arr2);
+
+		$arr3 = array('userId' => 3, 'password' => '789', 'fullName' => "ghi");
+		$json_encoded_3 = json_encode($arr3);
 
 		$f1 = fopen("temp.txt", "w");
-		fwrite($f1, $json_encoded . "\n");	
+		fwrite($f1, $json_encoded_1 . "\n");	
 		fclose($f1);
 
-		$f2 = fopen("temp.txt", "r");
-		$read =  fread($f2, filesize("temp.txt"));
+		$f2 = fopen("temp.txt", "a");
+		fwrite($f2, $json_encoded_2 . "\n");	
+		fclose($f2);
 
-		$json_decode = json_decode($read, true);
+		$f3 = fopen("temp.txt", "a");
+		fwrite($f3, $json_encoded_3 . "\n");	
+		fclose($f3);
 
-		echo $json_decode['userId'];
-		echo "<br>";
-		echo $json_decode['password'];
-		echo "<br>";
-		echo $json_decode['fullName'];
+		$f4 = fopen("temp.txt", "r");
+		$data =  fread($f4, filesize("temp.txt"));
+
+		$data_after_newline_delimeter = explode("\n", $data);
+
+		for($i = 0; $i < count($data_after_newline_delimeter) - 1; $i++) {
+			$json_decoded = json_decode($data_after_newline_delimeter[$i], true);
+			echo "<br>";
+			echo $json_decoded['userId'] . " " . $json_decoded['password']. " " . $json_decoded['fullName'];
+		}
 
 	?>
 
